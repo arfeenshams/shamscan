9373cd05c38f58eb2fa3faf734928d2bbbcc3e8e
arfeenshams <ishamskhan@yahoo.com>
2016-05-18 16:55:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -25,9 +25,9 @@ controller.setupWebserver(port, function (err, webserver) {
 
 
 controller.hears(['i need', 'send me', 'send to me', 'buy me', 'to my home', 'at my address', 'to my address'], 'message_received', function (bot, message) {
-  bot.reply(message, 'Send a list of your required products with your Information.')
-  bot.reply(message, 'Example: My Info. Mr. Iqbal Mustafa;, Cell # 0333 1234567;, House #55;, Street #23, New City Phase # 2, Wah cantt.')})
-
+  bot.reply(message, 'Enter your Products separated by commas (Example: Sugar 5kg, Dalda 5kg x 2, atta/flour 20kg, Marhaba Honey 500 grams, Dove Soap x 3, one facial moisturizer')
+  bot.reply(message, 'On what address should we deliver your products.')
+  bot.reply(message, 'Enter Your Active Cell Phone Number.')
 
 
 controller.hears(['hello', 'hi', 'hay', 'hey', 'salam', 'aoa', 'AOA', 'A.O.A'], 'message_received', function (bot, message) {
