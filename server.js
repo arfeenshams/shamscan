be9f8f5f96aa51fae2bc871fbc1672d0eecd46c7
arfeenshams <ishamskhan@yahoo.com>
2016-05-27 09:39:43 +0500

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ controller.hears(['billing address','shipping address', 'biling adres', 'shiping
   bot.reply(message, 'Billing address is where we collect the Amount')})
 
 
+controller.hears(['rfeen.com?', 'rfeen?', 'what is rfeen?', 'what is rfeen.com', 'what you do?', 'what do you Sell?'], 'message_received', function (bot, message) {
+  bot.reply(message, 'Visit our Website www.rfeen.com for more information')
+  bot.reply(message, 'rfeen.com is your one stop shop for anything you want ranging from grocery items to household products, from snacks & confectionary to goods of personal care. You will find everything under one roof virtually. rfeen.com is your ultimate destination for shopping online. Let’s do grocery.)
+  bot.reply(message, 'Call us at 0333 1542230')})
+  
 
 
 controller.hears(['hello', 'hi', 'hay', 'hey', 'salam', 'aoa', 'AOA', 'A.O.A'], 'message_received', function (bot, message) {
@@ -78,52 +83,5 @@ controller.hears(['cart', 'show my cart', 'show me cart', 'in my cart', 'my cart
   bot.reply(message, 'www.rfeen.com/cart')})
   
 controller.hears(['Help', 'help me', 'how to', 'to order', 'how order', 'can you help', 'Guide me', 'Need help', 'Need Guidance', 'How it works', 'How to Shop?'], 'message_received', function (bot, message) {
-  bot.reply(message, 'i will help you out')
-  bot.reply(message, 'Okay! here we go. Just follow these steps')
-  bot.reply(message, {
-    attachment: {
-      type: 'template',
-      payload: {
-        template_type: 'button',
-        text: 'Which do you prefer',
-        buttons: [
-          {
-            type: 'postback',
-            title: 'Cats',
-            payload: 'show_cat'
-          },
-          {
-            type: 'postback',
-            title: 'Dogs',
-            payload: 'show_dog'
-          }
-        ]
-      }
-    }
-  })
-})
-
-controller.on('facebook_postback', function (bot, message) {
-  switch (message.payload) {
-    case 'show_cat':
-      bot.reply(message, {
-        attachment: {
-          type: 'image',
-          payload: {
-            url: 'https://media.giphy.com/media/5xaOcLT4VhjRfudPcS4/giphy.gif'
-          }
-        }
-      })
-      break
-    case 'show_dog':
-      bot.reply(message, {
-        attachment: {
-          type: 'image',
-          payload: {
-            url: 'https://media.giphy.com/media/3o7ZeL5FH6Ap9jR9Kg/giphy.gif'
-          }
-        }
-      })
-      break
-  }
-})
+  bot.reply(message, '0333 1542230')
+  bot.reply(message, 'for any kind of help just Contact our admin')})
