4d9a4f7f0710cfd4c730c9a2bef0d42758778f14
arfeenshams <ishamskhan@yahoo.com>
2017-10-02 15:09:38 +0500

    Update server.js

--- server.js
+++ server.js
@@ -25,6 +25,12 @@ controller.setupWebserver(port, function (err, webserver) {
 controller.hears(['bhai', 'listen', 'hello', 'hi', 'hay', 'hey', 'salam', 'aoa', 'AOA', 'A.O.A'], 'message_received', function (bot, message) {
   bot.reply(message, 'Hi, How can i help you?. Please visit www.fallclub.com/events for our upcoming events. For Registration visit www.fallclub.com/join. To check out our Rates/Price List visit www.fallclub.com/Rates, (To talk to our person please leave a text at 0333 1542230)')})
 
+
+
+controller.hears(['Fallclub?', 'Fall Club ?', 'What is Fallclub?',], 'message_received', function (bot, message) {
+  bot.reply(message, 'It's your own Club, We Organize Events for you. Traveling, Family, Birthday etc. (To talk to our person please leave a text at 0333 1542230)')})
+
+
 controller.hears(['registeration', 'Register', 'registr', 'register me'], 'message_received', function (bot, message) {
   bot.reply(message, 'For Registration visit www.fallclub.com/join. (To talk to our person please leave a text at 0333 1542230)')})
 
@@ -42,7 +48,7 @@ controller.hears(['my name is'], 'message_received', function (bot, message) {
 controller.hears(['gandu', 'gando', 'bc', 'bhenchod', 'raami', 'fucker', 'fuck face', 'Fuck'], 'message_received', function (bot, message) {
   bot.reply(message, 'plz...b specific')})
 
-controller.hears(['Okay', 'okie', 'ok', 'okayy', 'okey'], 'message_received', function (bot, message) {
+controller.hears(['Okay', 'right', 'alright', 'okie', 'ok', 'okayy', 'okey'], 'message_received', function (bot, message) {
   bot.reply(message, ':)')})
 
 controller.hears(['Please share my Pictures', 'pics', 'give me my pics', 'Pictures', 'Aoa pics nae mili abi tak','please send pictures', 'please photos', 'please upload pics', 'sir pic to share karain', 'Pictures Please', 'Our Pic', 'Our Pictures', 'Our Photos'], 'message_received', function (bot, message) {
